fdce14fb42926431537eb852a73acdb5ab11feba
ARSHAD <62824836+arshad2002@users.noreply.github.com>
2025-01-02 12:02:22 +0600

    accepted with prefix sum

--- Count_Vowel_Strings_in_Ranges.py
+++ Count_Vowel_Strings_in_Ranges.py
@@ -3,12 +3,17 @@
 def vowelStrings(words, queries):
     vowel = set('aeiou')
     result = []
-    for j in range(len(queries)):
-        count=0
-        for i in range(queries[j][0],queries[j][1]+1):
-            if words[i][0] in vowel and words[i][-1] in vowel:
-                count+=1
-        result.append(count)
+    words_len = len(words)
+    prefix_sum = [0] * (words_len+1)
+
+    for i in range(words_len):
+        if words[i][0] in vowel and words[i][-1] in vowel:
+            prefix_sum[i+1] = prefix_sum[i]+1
+        else:
+            prefix_sum[i + 1] = prefix_sum[i]
+    for left,right in queries:
+        result.append(prefix_sum[right + 1] - prefix_sum[left])
+
     return result
 
                 
@@ -18,7 +23,7 @@ def vowelStrings(words, queries):
 
 
 if __name__ == '__main__':
-    words =["bzmxvzjxfddcuznspdcbwiojiqf","mwguoaskvramwgiweogzulcinycosovozppl","uigevazgbrddbcsvrvnngfrvkhmqszjicpieahs","uivcdsboxnraqpokjzaayedf","yalc","bbhlbmpskgxmxosft","vigplemkoni","krdrlctodtmprpxwditvcps","gqjwokkskrb","bslxxpabivbvzkozzvdaykaatzrpe","qwhzcwkchluwdnqjwhabroyyxbtsrsxqjnfpadi","siqbezhkohmgbenbkikcxmvz","ddmaireeouzcvffkcohxus","kjzguljbwsxlrd","gqzuqcljvcpmoqlnrxvzqwoyas","vadguvpsubcwbfbaviedr","nxnorutztxfnpvmukpwuraen","imgvujjeygsiymdxp","rdzkpk","cuap","qcojjumwp","pyqzshwykhtyzdwzakjejqyxbganow","cvxuskhcloxykcu","ul","axzscbjajazvbxffrydajapweci"]
-    queries =[[4, 4],[6, 17],[10, 17],[9, 18],[17, 22],[5, 23],[2, 5],[17, 21],[5, 17],[4, 8],[7, 17],[16, 19],[7, 12],[9, 20],[13, 23],[1, 5],[19, 19]]
+    words = ["aba","bcb","ece","aa","e"]
+    queries = [[0,2],[1,4],[1,1]]
     print(vowelStrings(words,queries))
     
