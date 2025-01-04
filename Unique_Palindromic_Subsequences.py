648df8aa57e0160bda8a25fb9f92e70e86b37b54
ARSHAD <62824836+arshad2002@users.noreply.github.com>
2025-01-04 23:43:10 +0600

    Faster version

--- Unique_Palindromic_Subsequences.py
+++ Unique_Palindromic_Subsequences.py
@@ -1,9 +1,8 @@
 # 1930. Unique Length-3 Palindromic Subsequences
 def countPalindromicSubsequence(s):
-    letters = set(s)
     count = 0
 
-    for l in letters:
+    for l in set(s):
         i,j = s.find(l),s.rfind(l)
         if j>i+1:
             count += len(set(s[i+1:j]))
